8bb6ce31eba4dd675b51de429b250a3daac134a7
Adriano Kaito <adrianokaito@Adrianos-MacBook-Pro.local>
2018-12-03 21:47:11 -0200

    commit

---
+++ Table View Controller/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Table View Controller
+//
+//  Created by Adriano Kaito on 03/12/18.
+//  Copyright © 2018 AK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var dados: [String] = ["Lasanha", "Pizza", "Torta"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dados.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let celulaReuso = "celulaReuso"
+        let celula = tableView.dequeueReusableCell(withIdentifier: celulaReuso, for: indexPath)
+        celula.textLabel?.text = dados[indexPath.row]
+        
+        return celula
+    }
+
+}
+
